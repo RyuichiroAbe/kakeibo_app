405fc6b9e258d5ed8a8b926494bc7bc8ab59bc20
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-07-13 20:32:37 +0900

    登録画面完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_055023) do
+ActiveRecord::Schema.define(version: 2020_07_13_111156) do
+
+  create_table "dixedcosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fixedcosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "incomes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "variablecosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.datetime "created_at", null: false
---
+++ test/models/dixedcost_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DixedcostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
